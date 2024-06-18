122e5f2eedce93145f102c4ad093125941db3ab7
Kazuma Hoda <saiteigundan@gmail.com>
2024-06-18 20:45:38 +0900

    fix: 引数の順番ミスを修正

--- start_newton.py
+++ start_newton.py
@@ -1,5 +1,6 @@
 import boto3
 import time
+from datetime import datetime
 from typing import Optional
 
 sqs = boto3.client("sqs")
@@ -21,10 +22,10 @@ def initialize(initial_count: int, concurrency: int):
         initial_count (int): 初期化時にキューに貯めておく件数
         concurrency (int): Lambda 関数の並列数
     """
-    print("トリガーの無効化を開始しました。")
+    print(now(), "トリガーの無効化を開始しました。")
     if not toggle_sqs_lambda_event(False, concurrency):
-        print("トリガーの無効化が完了しました。")
-        print("初期メッセージの送信を開始します。")
+        print(now(), "トリガーの無効化が完了しました。")
+        print(now(), "初期メッセージの送信を開始します。")
         for i in range(0, initial_count, MAX_BATCH_SIZE):
             entries = [
                 {
@@ -35,18 +36,18 @@ def initialize(initial_count: int, concurrency: int):
             ]
             sqs.send_message_batch(QueueUrl=queue_name, Entries=entries)
 
-        print("初期メッセージの送信が完了しました。")
+        print(now(), "初期メッセージの送信が完了しました。")
 
 
-def add_to_row(count_per_min: int):
+def add_to_row(count_per_min: int, concurrency: int):
     """キューに指定した件数のメッセージを毎分送信する
 
     Args:
         count_per_min (int): 毎分の送信件数
     """
     print("トリガーの有効化を開始しました。")
-    if toggle_sqs_lambda_event(True):
-        print("トリガーの有効化が完了しました。")
+    if toggle_sqs_lambda_event(True, concurrency):
+        print(now(), "トリガーの有効化が完了しました。")
         j = 0
         while True:
             for i in range(count_per_min):
@@ -56,7 +57,7 @@ def add_to_row(count_per_min: int):
                 )
                 time.sleep(60 / count_per_min)
 
-            print(f"{count_per_min} 件のメッセージ送信が完了しました。")
+            print(now(), f"{count_per_min} 件のメッセージ送信が完了しました。")
             # 1分ごとにキュー内に残っているメッセージを確認
             response = sqs.receive_message(QueueUrl=queue_name, VisibilityTimeout=0)
             messages = response.get("Messages")
@@ -69,13 +70,11 @@ def add_to_row(count_per_min: int):
                 ReceiptHandle=messages[0]["ReceiptHandle"],
                 VisibilityTimeout=0,
             )
-            print("キューにメッセージが残っているため、再度送信を開始します。")
+            print(now(), "キューにメッセージが残っているため、再度送信を開始します。")
             j += count_per_min
 
 
-def toggle_sqs_lambda_event(
-    enable: bool, concurrency: Optional[int] = None
-) -> Optional[bool]:
+def toggle_sqs_lambda_event(enable: bool, concurrency: int) -> Optional[bool]:
     """SQS の Lambda トリガーを有効化/無効化する
         concurrency が指定された場合、関数の最大並列数も変更する
 
@@ -88,12 +87,13 @@ def toggle_sqs_lambda_event(
     """
     response = _lambda.list_event_source_mappings(FunctionName=function_name)
     uuid = response["EventSourceMappings"][0]["UUID"]
-    arg = {"UUID": uuid, "FunctionName": function_name, "Enabled": enable}
-    # 並列数が指定されている場合はパラメータにセット
-    if concurrency is not None:
-        arg["ScalingConfig"] = {"MaximumConcurrency": concurrency}
 
-    response = _lambda.update_event_source_mapping(**arg)
+    response = _lambda.update_event_source_mapping(
+        UUID=uuid,
+        FunctionName=function_name,
+        Enabled=enable,
+        ScalingConfig={"MaximumConcurrency": concurrency}
+    )
     count = 0
     while True:
         time.sleep(5)
@@ -105,10 +105,10 @@ def toggle_sqs_lambda_event(
             time.sleep(10)
             return False
         elif count == 10:
-            print("待機回数が10回を超えたため、処理を中止します。")
+            print(now(), "待機回数が10回を超えたため、処理を中止します。")
             return
         else:
-            print("待機中...")
+            print(now(), "待機中...")
 
 
 def run(initial_count: int, increasing_rate: int, concurrency: int):
@@ -120,8 +120,17 @@ def run(initial_count: int, increasing_rate: int, concurrency: int):
         concurrency (int): キューを処理する Lambda 関数の最大並列数
     """
     initialize(initial_count, concurrency)
-    add_to_row(increasing_rate)
+    add_to_row(increasing_rate, concurrency)
+
+
+def now() -> str:
+    """現在時刻を出力する
+
+    Returns:
+        str: 現在時刻（ローカル時間）
+    """
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
 
 
 if __name__ == "__main__":
-    run(initial_count, initial_concurrency, increasing_rate)
+    run(initial_count, increasing_rate, initial_concurrency)
